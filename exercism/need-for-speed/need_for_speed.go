53f33d2b563ea69d9065ac1909839cb83c7d0a63
jrpespinas <jrpespinas@gmail.com>
2022-02-11 22:49:54 +0800

    chore: add Structs exercise

---
+++ exercism/need-for-speed/need_for_speed.go
@@ -0,0 +1,40 @@
+package speed
+
+// TODO: define the 'Car' type struct
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{speed: speed, batteryDrain: batteryDrain, battery: 100, distance: 0}
+}
+
+// TODO: define the 'Track' type struct
+type Track struct {
+	distance int
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{distance: distance}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive on more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery-car.batteryDrain > 0 {
+		car.distance = car.distance + car.speed
+		car.battery = car.battery - car.batteryDrain
+	}
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	mul := int(car.battery / car.batteryDrain)
+	return mul*car.speed >= track.distance
+}
