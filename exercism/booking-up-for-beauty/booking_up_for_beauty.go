5adb457511eaf66e76b85d5953215a9b10adb011
jrpespinas <jrpespinas@gmail.com>
2022-02-12 00:20:34 +0800

    chore: add Time exercise

---
+++ exercism/booking-up-for-beauty/booking_up_for_beauty.go
@@ -0,0 +1,35 @@
+package booking
+
+import (
+	"fmt"
+	"time"
+)
+
+// Schedule returns a time.Time from a string containing a date
+func Schedule(date string) time.Time {
+	t, _ := time.Parse("1/2/2006 15:04:05", date)
+	return t
+}
+
+// HasPassed returns whether a date has passed
+func HasPassed(date string) bool {
+	t, _ := time.Parse("January 2, 2006 15:04:05", date)
+	return t.Before(time.Now())
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon
+func IsAfternoonAppointment(date string) bool {
+	t, _ := time.Parse("Monday, January 2, 2006 15:04:05", date)
+	return t.Hour() >= 12 && t.Hour() <= 18
+}
+
+// Description returns a formatted string of the appointment time
+func Description(date string) string {
+	appointment := Schedule(date)
+	return fmt.Sprintf("You have an appointment on %v, %v %v, %v, at %v:%v.", appointment.Weekday(), appointment.Month(), appointment.Day(), appointment.Year(), appointment.Hour(), appointment.Minute())
+}
+
+// AnniversaryDate returns a Time with this year's anniversary
+func AnniversaryDate() time.Time {
+	return time.Date(time.Now().Year(), time.September, 15, 0, 0, 0, 0, time.UTC)
+}
