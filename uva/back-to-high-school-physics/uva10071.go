39cc6c9f081d0e170fad62bf679972504325463f
jrpespinas <jrpespinas@gmail.com>
2022-02-12 11:54:20 +0800

    chore: add 10071 uva exercise

--- uva/back-to-high-school-physics/uva10071.go
+++ uva/back-to-high-school-physics/uva10071.go
@@ -5,11 +5,9 @@ import (
 )
 
 func main() {
-	// initialize variables
 	var v, t int
 
 	_, err := fmt.Scan(&v, &t)
-	// Ask for inputs
 	for err == nil {
 		fmt.Println((v * t) * 2)
 		_, err = fmt.Scan(&v, &t)
