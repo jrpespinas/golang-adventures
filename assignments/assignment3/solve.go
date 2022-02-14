5a39368a41c90f9ad352645fd7d0433db3c5a4db
jrpespinas <jrpespinas@gmail.com>
2022-02-14 12:36:57 +0800

    refactor: simplify computeDeterminants function

--- assignments/assignment3/solve.go
+++ assignments/assignment3/solve.go
@@ -19,6 +19,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("url does not exist: %v", r.URL.Path)
 	}
 
+	// Parse coefficients
 	coefficients := getCoefficientsMatrix(w, r)
 
 	// Print systems of equation
@@ -27,9 +28,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%dx + %dy + %dz = %d\n", coefficients[j], coefficients[j+1], coefficients[j+2], coefficients[j+3])
 	}
 
-	// retrun solution
-	message := computeDeterminants(coefficients)
-	fmt.Fprintf(w, "\n%v", message)
+	// return solution
+	d, dx, dy, dz := computeDeterminants(coefficients)
+
+	if d != 0 {
+		fmt.Fprintf(w, "\nSolution:\nx = %.2f, y = %.2f, z = %.2f\n", dx/d, dy/d, dz/d)
+	} else if d == 0 && dx == 0 && dy == 0 && dz == 0 {
+		fmt.Fprintf(w, "\nDependent - With Multiple Solutions")
+	} else {
+		fmt.Fprintf(w, "\nInconsistent - No Solution")
+	}
 }
 
 func getCoefficientsMatrix(w http.ResponseWriter, r *http.Request) []int {
@@ -60,17 +68,11 @@ func getCoefficientsMatrix(w http.ResponseWriter, r *http.Request) []int {
 	return coefficients
 }
 
-func computeDeterminants(coefficients []int) string {
+func computeDeterminants(coefficients []int) (float64, float64, float64, float64) {
 	d := float64((coefficients[0] * (coefficients[5]*coefficients[10] - coefficients[6]*coefficients[9])) - (coefficients[4] * (coefficients[1]*coefficients[10] - coefficients[2]*coefficients[9])) + (coefficients[8] * (coefficients[1]*coefficients[6] - coefficients[2]*coefficients[5])))
 	dx := float64((coefficients[3] * (coefficients[5]*coefficients[10] - coefficients[6]*coefficients[9])) - (coefficients[7] * (coefficients[1]*coefficients[10] - coefficients[2]*coefficients[9])) + (coefficients[11] * (coefficients[1]*coefficients[6] - coefficients[2]*coefficients[5])))
 	dy := float64((coefficients[0] * (coefficients[7]*coefficients[10] - coefficients[6]*coefficients[11])) - (coefficients[4] * (coefficients[3]*coefficients[10] - coefficients[2]*coefficients[11])) + (coefficients[8] * (coefficients[3]*coefficients[6] - coefficients[2]*coefficients[7])))
 	dz := float64((coefficients[0] * (coefficients[5]*coefficients[11] - coefficients[7]*coefficients[9])) - (coefficients[4] * (coefficients[1]*coefficients[11] - coefficients[3]*coefficients[9])) + (coefficients[8] * (coefficients[1]*coefficients[7] - coefficients[3]*coefficients[5])))
 
-	if d != 0 {
-		return fmt.Sprintf("Solution:\nx = %.2f, y = %.2f, z = %.2f\n", dx/d, dy/d, dz/d)
-	} else if d == 0 && dx == 0 && dy == 0 && dz == 0 {
-		return fmt.Sprintf("Dependent - With Multiple Solutions")
-	} else {
-		return fmt.Sprintf("Inconsistent - No Solution")
-	}
+	return d, dx, dy, dz
 }
