36bd210600051b521b07574cd6f22c3abc003f70
Bogs Espinas <jrpespinas@gmail.com>
2022-04-21 13:54:47 +0800

    feat: dockerize backend

--- final-project-2/server/app.go
+++ final-project-2/server/app.go
@@ -10,15 +10,7 @@ import (
 
 func main() {
 	// Get port number
-	port := config.GetPort(config.Getenv("PORT"))
-
-	// Start Logger
-	f, err := os.OpenFile("logs/logs.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("ERROR: %v", err.Error())
-	}
-	log.SetOutput(f)
-	defer f.Close()
+	port := config.GetPort(os.Getenv("PORT"))
 
 	log.Printf("SERVER: Listening at port%v", port)
 	http.ListenAndServe(port, route.Router())
