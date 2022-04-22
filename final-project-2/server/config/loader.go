e3c04875dd7f7961e913ff8ebd91363d72c0b35e
Bogs Espinas <jrpespinas@gmail.com>
2022-04-22 11:36:38 +0800

    refactor: change default port number

--- final-project-2/server/config/loader.go
+++ final-project-2/server/config/loader.go
@@ -8,7 +8,7 @@ import (
 // Return default PORT number
 func GetPort(port string) string {
 	if port == "" {
-		return ":8000"
+		return ":3333"
 	} else {
 		return fmt.Sprintf(":%v", os.Getenv("PORT"))
 	}
